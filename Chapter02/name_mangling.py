498c9e4367278c7234b1c2fbe2cd450f5290ef43
Mustafa <mustafa.sheikhj@gmail.com>
2019-03-07 23:03:16 -0500

    Added a module for testing purposes in teh chapter and also made some changes to the name_mangling.py file by adding some example classes to it.

--- Chapter02/name_mangling.py
+++ Chapter02/name_mangling.py
@@ -11,3 +11,31 @@ class SecretString:
             return self.__plain_string
         else:
             return ""
+
+class NotSecretString:
+    """A not-at-all secure way to store a secret string."""
+
+    def __init__(self, plain_string, pass_phrase):
+        self._plain_string = plain_string
+        self._pass_phrase = pass_phrase
+
+    def decrypt(self, pass_phrase):
+        """Only show the string if the pass_phrase is correct."""
+        if pass_phrase == self._pass_phrase:
+            return self._plain_string
+        else:
+            return ""
+        
+class PublicString:
+    """A not-at-all secure way to store a secret string."""
+
+    def __init__(self, plain_string, pass_phrase):
+        self.plain_string = plain_string
+        self.pass_phrase = pass_phrase
+
+    def decrypt(self, pass_phrase):
+        """Only show the string if the pass_phrase is correct."""
+        if pass_phrase == self.pass_phrase:
+            return self.plain_string
+        else:
+            return ""
---
+++ main.py
@@ -0,0 +1,2 @@
+import ecommerce.products
+product = ecommerce.products.Product()
